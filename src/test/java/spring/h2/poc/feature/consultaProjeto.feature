399463c410879da1e5141cb60250d10d6c67afd7
wmcosta <wmcosta@latam.stefanini.com>
2018-09-19 13:44:22 -0300

    classes de feature e step

--- src/test/java/spring/h2/poc/feature/consultaProjeto.feature
+++ src/test/java/spring/h2/poc/feature/consultaProjeto.feature
@@ -6,8 +6,7 @@ Funcionalidade: Consultar Projeto
   COMO Usuário DESEJO consultar todos os projetos cadastrados.
   
 	Cenario: Consultar Projeto  
-		Dado eu um Usuário 
-		Quando consultar todos os projetos 
-		Entao o Sistema deverá apresentar os campos de busca
-		Entao os projetos sejam ordenados por ordem alfabética crescente por nome 
-		Entao eu os projetos acessados apresentem as opções de #AlterarProjeto, #ExcluirProjeto e #DetalharProjeto.
+		Dado que eu acesse a aplicação
+		E informar o nome do projeto "Stefanini"
+		Quando eu clicar em Pesquisar
+		Entao a aplicação deverá apresentar a lista de projetos em ordem crescente
--- src/test/java/spring/h2/poc/step/ConsultaProjetoStep.java
+++ src/test/java/spring/h2/poc/step/ConsultaProjetoStep.java
@@ -1,30 +1,27 @@
 package spring.h2.poc.step;
 
-import cucumber.api.PendingException;
-import cucumber.api.Scenario;
-import cucumber.api.java.Before;
 import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.Entao;
 import cucumber.api.java.pt.Quando;
 
 public class ConsultaProjetoStep {
-	
-	private Scenario scenario;
-	
-	@Before
-	public void before(Scenario scenario) {
-		this.scenario = scenario;
-	}
-	
-	@Dado("^eu um Usuário$")
-	public void eu_um_Usuário() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+
+	@Dado("^que eu acesse a aplicação$")
+	public void que_eu_acesse_a_aplicação() throws Throwable {
 	}
 
-	@Quando("^consultar todos os projetos$")
-	public void consultar_todos_os_projetos() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	@Dado("^informar o nome do projeto \"([^\"]*)\"$")
+	public void informar_o_nome_do_projeto(String arg1) throws Throwable {
+
 	}
-	
+
+	@Quando("^eu clicar em Pesquisar$")
+	public void eu_clicar_em_Pesquisar() throws Throwable {
+
+	}
+
+	@Entao("^a aplicação deverá apresentar a lista de projetos em ordem crescente$")
+	public void a_aplicação_deverá_apresentar_a_lista_de_projetos_em_ordem_crescente() throws Throwable {
+	}
+
 }
